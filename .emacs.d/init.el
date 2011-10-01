6f60e5170a3e3d094ae300ffb574734bb620b79e
Jason Hamilton <laydros@gmail.com>
2011-10-01 01:15:06 -0400

    emacs config, needs t60 stuff

---
+++ .emacs.d/init.el
@@ -0,0 +1,9 @@
+
+
+(tool-bar-mode nil)
+(setq column-number-mode t)
+(global-set-key "\C-x\C-b" 'electric-buffer-list)
+(display-time-mode t)
+(display-battery-mode t)
+(scroll-bar-mode nil)
+(menu-bar-mode nil)
