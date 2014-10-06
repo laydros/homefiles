713c7e6f07418967f222b9c74b6c8a0ed0bb9737
Jason Hamilton <jwh@laydros.net>
2014-10-06 19:59:44 -0400

    updated emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,160 +1,127 @@
+;;; init.el
 
+;; Author: Jason Hamilton
+
+;;; Commentary:
+
+;;; Code:
+
+;; ************************************************************************
+;; * initial display stuff so things don't flash all funky
+;; ************************************************************************
 ;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(blink-cursor-mode -1)
+	
+;;(set-default-font "Terminus-10")
+(set-default-font "Inconsolata-10")
 
+;; ************************************************************************
+;; *  package manager
+;; ************************************************************************
 (require 'package)
-(require 'json)
-
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-;;Initialize package mode along with all the installed packages
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
 (package-initialize)
 
-(defun jwh-require-package (name)
+(require 'zenburn-theme)
+
+(defun laydros-require-package (name)
   (unless (package-installed-p name)
     (package-install name)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;    projectile
-;; ------------------------
-;; C-c p s - switch to a project
-;; C-c p f - list files in a project
-;; C-c p k - kill all buffers for a project
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(jwh-require-package 'projectile)
-(require 'projectile)
-(projectile-global-mode)
-(setq projectile-completion-system 'ido)
+;; ************************************************************************
+;; * - Elpy -
+;; ************************************************************************
+(laydros-require-package 'elpy)
+(elpy-enable)
+(elpy-use-ipython)
 
-;;;;;;;;;;;;;;;;;;;;;;;;
-;; auto-complete
-;;;;;;;;;;;;;;;;;;;;;;;;
-(jwh-require-package 'auto-complete)
+;; ************************************************************************
+;; * - Auto-complete -
+;; ************************************************************************
+(laydros-require-package 'auto-complete)
 (require 'auto-complete-config)
 (ac-config-default)
-;; show menu very quickly - may need to turn back off
-(setq ac-show-menu-immediately-on-auto-complete t)
 
-;; (setq jwh-init-dir (expand-file-name "init.d"))
+;; company is supposed to be better, but it is getting on my nerves
+;; jwh-2014-10-06
+; (laydros-require-package 'company)
+; (require 'company)
+
+;; (setq company-idle-delay 0.5)
+;; (setq company-tooltip-limit 10)
+;; (setq company-minimum-prefix-length 2)
+;; ;; invert the navigation direction if the completion popup-isearch-match
+;; ;; is displayed on top (happens near the bottom of the windows)
+;; (setq company-tooltip-flip-when-above 1)
+
+;; (global-company-mode 1)
+
+;; ************************************************************************
+;; * - ido -
+;; * Ido mode provides a nice way to navigate the filesystem. This is mostly
+;; * just turning it on.
+;; ************************************************************************
+
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ibuffer-shrink-to-minimum-size t
+      ibuffer-sorting-mode 'recency
+      ido-use-virtual-buffers t)
+(laydros-require-package 'ido-vertical-mode)
+(ido-vertical-mode t)
 
 
-(jwh-require-package 'monokai-theme)
-(load-theme 'monokai t)
+;; ************************************************************************
+;; * - General -
+;; ************************************************************************
+(setq user-mail-address "jwh@laydros.net")
 
-;; load all lisp files in ./init.d
-;; (if (file-exists-p jwh-init-dir)
-;;     (dolist (file (directory-files jwh-init-dir t "\\.el$"))
-;;       (load-file)))
+;; run in server mode
+(server-start)        
 
+;; in order to load init other init files
+(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+
+;; (require 'molokai-theme)
+
+(laydros-require-package 'autopair)
+(require 'autopair)
 
 (setq visible-bell t)
-
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-(blink-cursor-mode 0)
 (display-time-mode 1)
+
+;; I always use 4 tabs as spaces. this is PEP-8 and feels right for other stuff
 (setq standard-indent 4)
 (setq-default indent-tabs-mode nil)
-(setq scroll-step 1)
 
-(setq transient-mark-mode t) ; visually show region
-(setq global-font-lock-mode 1) ; everything should use fonts
-
-;(jwh-require-package 'ipython)
-
-(jwh-require-package 'elpy)
-(jwh-require-package 'flymake-cursor)
-(jwh-require-package 'magit)
-
-;;(elpy-enable)
-;;(elpy-clean-modeline)
-;; Fixing a key binding bug in elpy
-;(define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
-; Fixing another key binding bug in iedit mode
-(define-key global-map (kbd "C-c o") 'iedit-mode)
-
-(defun python--add-debug-highlight ()
-  "Adds a highlighter for use by `python--ipdb-breakpoint-string'"
-  (highlight-lines-matching-regexp "## DEBUG ##\\s-*$" 'hi-red-b))
-
-(add-hook 'python-mode-hook 'python--add-debug-highlight)
-
-(defvar python--ipdb-breakpoint-string "import ipdb; ipdb.set_trace() ## DEBUG ##"
-  "Python breakpoint string used by `python-insert-breakpoint'")
-
-;; (defun python-insert-breakpoint ()
-;;   "Inserts a python breakpoint using `pdb'"
-;;   (interactive)
-;;   (back-to-indentation)
-;;   ;; this preserves the correct indentation in case the line above
-;;   ;; point is a nested block
-;;   (split-line)
-;;   (insert python--ipdb-breakpoint-string))
-;(define-key python-mode-map (kbd "<f6>") 'python-insert-breakpoint)
-
-
-(highlight-indentation-mode -1)
-
-;(require 'bbdb)
+(setq transient-mark-mode t)
+(setq global-font-lock-mode 1)
 
 (line-number-mode t)
 (column-number-mode t)
+(global-linum-mode t)
+(size-indication-mode t)
 
-(setq-default fill-column 79)
-
-(show-paren-mode t)
-(jwh-require-package 'autopair)
-(require 'autopair)
-
-(jwh-require-package 'auto-complete)
-(require 'auto-complete-config)
-(ac-config-default)
-
-;;(jwh-require-package 'undo-tree)
-;;(require 'undo-tree)
-;;(global-undo-tree-mode)
-
-;(jwh-require-package 'back-button)
-;(require 'back-button)
-;(back-button-mode 1)
-
-(jwh-require-package 'fill-column-indicator)
-(require 'fill-column-indicator)
+;; fill column tells me where to stop. PEP-8 calls for 80 columns or less, and
+;; I just let this carry into all of my text editing
+(laydros-require-package 'fill-column-indicator)
 (define-globalized-minor-mode
  global-fci-mode fci-mode (lambda () (fci-mode 1)))
 (global-fci-mode t)
+(setq-default fill-column 79)
 
-(jwh-require-package 'popwin)
-(require 'popwin)
-(popwin-mode 1)
+(show-paren-mode t)
 
-(highlight-indentation-mode -1)
+;; for tramp
+(setq tramp-default-method "ssh")
 
-;; resize window
-(global-set-key (kbd "M-<up>") 'enlarge-window)
-(global-set-key (kbd "M-<down>") 'shrink-window)
-
-;; sample function
-;; interactive makes it a command that can be run
-;(defun display-buffer-name ()
-;  (interactive)
-;  (message (buffer-file-name)))
-;(global-set-key (kbd "<f7>") 'display-buffer-name)
-;; same thing inline
-(global-set-key (kbd "<f8>") (lambda () (interactive) (message (buffer-file-name))))
-
-(jwh-require-package 'markdown-mode)
-(require 'markdown-mode)
-;; only in markdown mode, f9 will spell check
-(define-key markdown-mode-map (kbd "<f9>") 'ispell-buffer)
-(add-hook 'markdown-mode-hook 'flyspell-mode)
-
-(define-key elpy-mode-map (kbd "<f5>") 'flymake-display-err-menu-for-current-line)
-
-;; C-h f - get function help
-;; C-h v - get variable help
+(auto-compression-mode 1)
 
 ;; watch visited files for changes and reload if needed
 (setq
@@ -176,14 +143,6 @@
 
 ")
 
-
-;; -------- things i might want ---------
-;; typing when mark is active will delete highlighted
-; (delete-selection-mode t)
-;; common highlighting keystrokes
-;; mix emacs and x keyboard (i don't seem to need this)
-; (setq x-select-enable-clipboard t)
-
 ;; get rid of backup files
 (setq make-backup-files nil)
 
@@ -201,39 +160,6 @@ region\) apply comment-or-uncomment to the current line"
         (comment-or-uncomment-region (point) (mark))
       (comment-or-uncomment-region (mark) (point)))))
 
-;; ----------------------------------
-;; ---------- Key Bindings ----------
-;; ----------------------------------
-
-(global-set-key (kbd "RET") 'newline-and-indent)
-;; (global-set-key (kbd "C-;") 'comment-or-uncomment-region)
-(global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-+") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
-(global-set-key "\C-x\C-b" 'electric-buffer-list)
-(global-set-key "\C-c\C-r" 'comment-or-uncomment-region-or-line)
-(global-set-key (kbd "C-;") 'comment-or-uncomment-region-or-line)
-
-;; ----------------------------------
-;; ------------ Org-mode ------------
-;; ----------------------------------
-
-(global-set-key (kbd "C-c a") 'org-agenda)
-(setq org-log-done t)
-(setq org-todo-keywords
-      '((sequence "TODO" "INPROGRESS" "WAITING" "DONE")))
-(setq org-todo-keyword-faces
-      '(("INPROGRESS" . (:foreground "blue" :weight bold))))
-(setq org-agenda-files (list "~/Dropbox/org-mode/personal.org"))
-
-
-;; Ido mode provides a nice way to navigate the filesystem. This is mostly just turning it on.
-(ido-mode t)
-(setq ido-enable-flex-matching t
-      ido-use-virtual-buffers t)
-(jwh-require-package 'ido-vertical-mode)
-(ido-vertical-mode t)
-
 ;; makes temporary files go away
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
@@ -246,30 +172,6 @@ region\) apply comment-or-uncomment to the current line"
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
-;; ----------------------------------
-;; -              UTF-8             -
-;; ----------------------------------
-
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-
-
-(defun insertdate ()
-  "Spit out the current time"
-  (interactive)
-  (insert (format-time-string "%Y-%m-%d")))
-
-(global-set-key [(f5)] 'insertdate)
-
-
-;; (defun jonnay-sign ()                                        ;;
-;;   "spit out my name, email and the current time"             ;;
-;;   (interactive)                                              ;;
-;;   (insert "-- Jonathan Arkell (jonathana@criticalmass.com)") ;;
-;;   (jonnay-timestamp))                                        ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; never quit accidentally
 (setq confirm-kill-emacs 'yes-or-no-p)
@@ -280,87 +182,32 @@ region\) apply comment-or-uncomment to the current line"
 ;; Recent files
 (recentf-mode 1)
 
-;; yank at point
-(setq mouse-yank-at-point t)
+;; ************************************************************************
+;; * - UTF-8 -
+;; ************************************************************************
 
-(setq kill-whole-line t)
-
-(setq user-mail-address "jwh@laydros.net")
-
-(setq display-time-mode t)
-
-(global-linum-mode 1)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 
 
-;;;;;;;;;;;;;
-;; ERC stuff
-;;;;;;;;;;;;
-(setq erc-fill-function 'erc-fill-static)
-(setq erc-fill-static-center 22)
+;; ************************************************************************
+;; * - Key Bindings -
+;; ************************************************************************
+(global-set-key (kbd "RET") 'newline-and-indent)
+;; (global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key "\C-x\C-b" 'electric-buffer-list)
+(global-set-key "\C-c\C-r" 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "C-c ;") 'comment-or-uncomment-region-or-line)
 
-(erc :server "irc.freenode.net" :port 6667 :nick "laydros")
+;; ************************************************************************
+;; * - External loads -
+;; * stuff in external init files
+;; ************************************************************************
 
-(add-hook 'erc-after-connect '(lambda ("freenode.net")
-               (erc-message "PRIVMSG" "NickServ identify mypassword")))
-
-(setq erc-autojoin-channels-alist
-  '(("freenode.net" "#emacs" "#cm6-snap" "#nethack")))
-
-(setq erc-hide-list '("JOIN" "PART" "QUIT"))
-
-;;;;;;;;;;;;;;;;;;;
-;; Beautify JSON ;;
-;;;;;;;;;;;;;;;;;;;
-(defun beautify-json ()
-  (interactive)
-  (let ((b (if mark-active (min (point) (mark)) (point-min)))
-        (e (if mark-active (max (point) (mark)) (point-max))))
-    (shell-command-on-region b e
-      "python -mjson.tool" (current-buffer) t)))
-
-
-
-;; Enable Markdown mode and setup additional file extensions.
-;; Use pandoc to generate HTML previews from within the mode.
-;; Use a custom css file to make it a little prettier.
-
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
-(add-hook 'markdown-mode-hook (lambda () (visual-line-mode t)))
-;(setq markdown-command "pandoc --smart -f markdown -t html")
-;(setq markdown-css-path (expand-file-name "markdown.css" abedra/vendor-dir))
-
-(defun totd ()
-  (interactive)
-  (random t) ;; seed with time-of-day
-  (with-output-to-temp-buffer "*Tip of the day*"
-    (let* ((commands (loop for s being the symbols
-                           when (commandp s) collect s))
-           (command (nth (random (length commands)) commands)))
-      (princ
-       (concat "Your tip for the day is:\\n"
-               "========================\\n\\n"
-               (describe-function command)
-               "\\n\\nInvoke with:\\n\\n"
-               (with-temp-buffer
-                 (where-is command t)
-                 (buffer-string)))))))
-
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-(when (require 'em-term)
-  (dolist (i '("kop" "ledger" "mc" "htop"))
-    (add-to-list 'eshell-visual-commands i)))
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; load org-mode stuff
+(require 'laydros-org)
---
+++ emacs.d/laydros-org.el
@@ -0,0 +1,47 @@
+;;; laydros-org.el ___
+
+;; Author: Jason Hamilton
+;; Keywords:
+
+;; Keywords:
+ 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+ 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+ 
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+ 
+;;; Commentary:
+ 
+;;
+ 
+;;; Code:
+
+(custom-set-variables
+ '(auto-raise-tool-bar-buttons t t)
+ '(auto-resize-tool-bars t t)
+ '(calendar-week-start-day 1)
+ '(case-fold-search t)
+ '(make-backup-files nil)
+ '(org-agenda-files (quote ("/home/laydros/Documents/org/personal.org"
+                            "/home/laydros/Documents/org/work.org")))
+ '(org-agenda-show-all-dates t)
+ '(org-tags-column -78))
+
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-directory "/home/laydros/Dropbox/Documents/org")
+
+
+(provide 'laydros-org)
+;;; laydros-org.el ends here
