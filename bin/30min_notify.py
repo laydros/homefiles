ae73188753e291edb5940acf670e8367a5a51e30
Jason Hamilton <jwh@laydros.net>
2014-10-07 10:54:37 -0400

    renamed python notifier and set times for 30 minutes. changed body message to string that I will reset later

---
+++ bin/30min_notify.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import time
+from gi.repository import Notify
+Notify.init('Check Tasks')
+notify_body = "eventually this will pull from org agenda"
+Hello=Notify.Notification.new('Task Check','notify_body', 'dialog-information')
+# show for 5 minutes
+Hello.set_timeout(500000)
+while True:
+    # sleep for the remaining seconds until the half hour
+    time.sleep(1800-time.time()%1800)
+    Hello.show()
