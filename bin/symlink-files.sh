e04e2df1a918916ea7e31ddaa7e8fcf09a0d071c
Jason Hamilton <jwh@laydros.net>
2015-03-07 09:58:23 -0500

    updated sshconfig to have ibanez. symlink files fixed emacs lisp folder name, added gitignore and zshrc

--- bin/symlink-files.sh
+++ bin/symlink-files.sh
@@ -3,7 +3,7 @@
 echo "Creating symlinks for dotfiles:"
 
 echo "...create emacs dirs"
-mkdir -p ~/.emacs.d/lisp
+mkdir -p ~/.emacs.d/laydros-lisp
 
 echo "...emacs.d/init.el, laydros-org.el, org-contacts.el"
 ln -s ~/code/homefiles/emacs.d/init.el ~/.emacs.d/init.el
@@ -12,8 +12,9 @@ ln -s ~/code/homefiles/emacs.d/laydros-lisp ~/.emacs.d/laydros-lisp
 echo "...bin directory"
 ln -s ~/code/homefiles/bin ~/bin
 
-echo "...gitconfig"
+echo "...gitconfig and gitignore"
 ln -s ~/code/homefiles/gitconfig ~/.gitconfig
+ln -s ~/code/homefiles/gitignore_global ~/.gitignore_global
 
 echo "...i3 dir and i3status.conf"
 ln -s ~/code/homefiles/i3status.conf ~/.i3status.conf
@@ -34,3 +35,6 @@ ln -s ~/code/homefiles/vimrc ~/.vimrc
 
 echo "...Xresources"
 ln -s ~/code/homefiles/Xresources ~/.Xresources
+
+echo "...zshrc"
+ln -s ~/code/homefiles/zshrc ~/.zshrc
