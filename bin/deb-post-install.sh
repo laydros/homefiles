0ecbdfeb7ad3d991e5fcf4bfed65557612f752b1
Jason Hamilton <jwh@laydros.net>
2015-04-09 14:42:27 -0400

    added ibanez to ssh config. updated debian post-install script

--- bin/deb-post-install.sh
+++ bin/deb-post-install.sh
@@ -2,12 +2,11 @@
 
 SOURCES_LIST=""
 
-
 # List of packages to install
 LIST=""
 
 # media players
-LIST=$LIST" vlc ffmpeg spotify-client"
+LIST=$LIST" vlc spotify-client"
 
 # media codecs
 LIST=$LIST" gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly"
@@ -16,10 +15,10 @@ LIST=$LIST" gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer0.10-p
 LIST=$LIST" flashplugin-nonfree"
 
 # cli utils
-LIST=$LIST" curl wget git vim"
+LIST=$LIST" curl wget git vim ncdu"
 
 # common dev tools
-LIST=$LIST" geany meld gitk diffuse"
+LIST=$LIST" geany gitk xbindkeys diffuse"
 
 # python
 LIST=$LIST" python-pip idle-python2.7 python-rope python-dev"
@@ -30,33 +29,40 @@ LIST=$LIST" rxvt-unicode zsh tmux"
 # archive
 LIST=$LIST" p7zip unace-nonfree unrar unzip zip"
 
+# allow debian to install ppas
+LIST=$LIST" software-properties-common python-software-properties "
+
 # graphics and fonts
 LIST=$LIST" gimp ttf-bitstream-vera ttf-dejavu ttf-freefont ttf-mscorefonts-installer gtk2-engines-murrine"
 
 # Diodon clipboard manager
-LIST=$LIST" diodon diodon-plugins"
+#LIST=$LIST" diodon diodon-plugins"
 
+# Parcellite clibboard manager
+LIST=$LIST" parcellite"
 
+# awesome
+LIST=$LIST" galculator "
 
 #----------------------
 
 # add spotify to sources.list
 echo "Adding Spotify repo and key"
 echo ""
-# echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list
-# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
-apt-get update
+#echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
+#apt-get update
 
-# clear
+clear
 
-# echo "Upgrading existing packages before install..."
-# echo ""
-# apt-get upgrade
-# echo ""
-# sleep 2s
-# echo ""
-# apt-get dist-upgrade
+#echo "Upgrading existing packages before install..."
+#echo ""
+#apt-get upgrade
+#echo ""
+#sleep 2s
 echo ""
+#apt-get dist-upgrade
+#echo ""
 sleep 2s
 #---------------------
 clear
