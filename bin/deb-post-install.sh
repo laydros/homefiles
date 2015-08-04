4fcf833c8c99e9c5dfe32395728e29f3ce7a5238
Jason Hamilton <jwh@laydros.net>
2015-08-04 13:07:48 -0400

    update deb-post-install script

--- bin/deb-post-install.sh
+++ bin/deb-post-install.sh
@@ -5,6 +5,9 @@ SOURCES_LIST=""
 # List of packages to install
 LIST=""
 
+# firmware (must have non-free enabled)
+LIST=$LIST" intel-microcode firmware-linux"
+
 # media players
 LIST=$LIST" vlc spotify-client"
 
@@ -18,22 +21,28 @@ LIST=$LIST" flashplugin-nonfree"
 LIST=$LIST" curl wget git vim ncdu rdesktop"
 
 # common dev tools
-LIST=$LIST" geany gitk xbindkeys diffuse autokey-gtk"
+LIST=$LIST" geany gitk xbindkeys diffuse autokey-gtk build-essential"
 
 # python
-LIST=$LIST" python-pip idle-python2.7 python-rope python-dev"
+LIST=$LIST" python-pip python-rope python-dev"
 
 # shell stuff
 LIST=$LIST" rxvt-unicode zsh tmux"
 
 # archive
-LIST=$LIST" p7zip unace-nonfree unrar unzip zip"
+LIST=$LIST" p7zip unace-nonfree unrar unzip zip cifs-utils"
 
 # allow debian to install ppas
 LIST=$LIST" software-properties-common python-software-properties "
 
-# graphics and fonts
-LIST=$LIST" gimp ttf-bitstream-vera ttf-dejavu ttf-freefont ttf-mscorefonts-installer gtk2-engines-murrine"
+# keyboard
+LIST=$LIST" xbindkeys"
+
+# graphics
+LIST=$LIST" gimp gtk2-engines-murrine"
+
+# fonts
+LIST=$LIST" ttf-bitstream-vera ttf-dejavu ttf-freefont ttf-mscorefonts-installer ttf-liberation xfonts-terminus"
 
 # Diodon clipboard manager
 #LIST=$LIST" diodon diodon-plugins"
@@ -46,7 +55,7 @@ LIST=$LIST" parcellite"
 LIST=$LIST" libreoffice-style-sifr "
 
 # ubuntu only
-LIST=$LIST" unity-tweak-tool ubuntu-restricted-extras gnome-sushi "
+#LIST=$LIST" unity-tweak-tool ubuntu-restricted-extras gnome-sushi "
 
 # awesome
 LIST=$LIST" galculator "
@@ -58,22 +67,22 @@ echo "Allow gnome resize with right mouse button"
 gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 
 # add spotify to sources.list
-#echo "Adding Spotify repo and key"
-#echo ""
-#echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list
-#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
-#apt-get update
+echo "Adding Spotify repo and key"
+echo ""
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+#echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+apt-get update
 
 clear
 
-#echo "Upgrading existing packages before install..."
-#echo ""
-#apt-get upgrade
-#echo ""
-#sleep 2s
+echo "Upgrading existing packages before install..."
 echo ""
-#apt-get dist-upgrade
-#echo ""
+apt-get upgrade
+echo ""
+sleep 2s
+echo ""
+apt-get dist-upgrade
+echo ""
 sleep 2s
 #---------------------
 clear
@@ -81,3 +90,11 @@ echo "Installing package list..."
 echo "$LIST"
 apt-get install $LIST
 echo ""
+
+echo "Now that zsh and curl are installed, grabbing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Go install source code pro: https://github.com/adobe-fonts/source-code-pro"
+echo "mkdir -p ~/.fonts"
+echo "move OpenType fonts (.otf) or TrueType files (.ttf) to ~/.fonts"
+echo "sudo fc-cache"
