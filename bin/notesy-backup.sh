e54196db19b3a8ce782d511bedec1b3b2b5972dc
Jason Hamilton <jwh@laydros.net>
2015-12-18 15:21:19 -0500

    cleaning up some things. added scrot and tunnel_burn3 scripts, and new notesy backup script

---
+++ bin/notesy-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Daily org file backup
+
+# needed variables
+day=$(date +%F)
+rootPath="/home/laydros/Dropbox"
+notesyDir=notesy/
+backupFile=$rootPath/notesy-backup/$day.tar.gz
+
+# Backup files
+tar cpzf $backupFile -C $rootPath $notesyDir
+
+# remove backup files older than 90 days
+#find $backupFile* -mtime +90 -exec rm {} \;
+
+# to exclude certain directories
+#tar cpzf $File --exclude "${Folder}Downloads" $Folder
