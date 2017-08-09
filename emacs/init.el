74da9661e2500e640f4b939c563cbec1c9e70986
Jason Hamilton <jwh@laydros.net>
2017-08-09 19:05:56 -0400

    working on simpler emacs config. Created shell script to setup mac machine

--- emacs/init.el
+++ emacs/init.el
@@ -8,28 +8,18 @@
    t)
     (package-initialize))
 
-(setq make-backup-files nil)
-(setq-default indent-tabs-mode nil)
-(setq-default c-basic-offset 4)
-(setq c-basic-offset 4)
-(setq c-indent-level 4)
-(setq tab-width 4)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-(setq-default fill-column 80)
-
-(define-key global-map (kbd "RET") 'newline-and-indent)
-
-(display-time-mode t)
-(line-number-mode t)
-(column-number-mode t)
-(size-indication-mode t)
-
-;; end basics
-
-(set-default-font "Knack Nerd Font-11")
-
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/laydros-lisp"))
+
+(require 'laydros-base)
+
+(set-default-font "Iosevka Nerd Font-12")
+
+;; bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
 
 (load-theme 'solarized-dark t)
 
@@ -42,21 +32,17 @@
 (prefer-coding-system        'utf-8)
 
 ;; some interface niceties
-(setq confirm-kill-emacs                  'y-or-n-p
-      confirm-nonexistent-file-or-buffer  t
+(setq confirm-nonexistent-file-or-buffer  t
       mouse-yank-at-point                 t
-      inhibit-startup-message             t
       inhibit-startup-echo-area-message   t)
-
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
 
 ;; by default tooltips open in seperate frame. to force emacs to use the echo
 ;; area exclusively uncomment the following
 ;; (tooltip-mode -1)
 ;; (setq tooltip-use-echo-area t)
 
-;;(require 'laydros-base)
+(use-package company-mode
+  :ensure t)
 
 ;; helps to actually look at the documentation. Use M-n and M-p to select,
 ;; <return> to complete or <tab> to complete the common part. Search through
@@ -71,7 +57,23 @@
 ;; may want to get rid of this
 (setq ido-everywhere t)
 
+;; simply use M-o to switch windows, since this is done so often. Could also
+;; use (windmove-default-keybindings) to make S-up, S-dn, etc. move.
+(global-set-key (kbd "M-o") 'other-window)
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; use the system trash
 (setq delete-by-moving-to-trash t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package solarized-theme company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs/laydros-lisp/laydros-base.el
+++ emacs/laydros-lisp/laydros-base.el
@@ -1,29 +1,29 @@
 ;; laydros-base.el
-;; basic setup stuff
+;; basic setup stuff - could be used as a minimal init.el
 ;; last edited 2017-07-25
 
-;; UTF-8
-(set-charset-priority 'unicode)
-(setq locale-coding-system   'utf-8)
-(set-terminal-coding-system  'utf-8)
-(set-keyboard-coding-system  'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system        'utf-8)
+(setq make-backup-files nil)
+(setq-default indent-tabs-mode nil)
+(setq-default c-basic-offset 4)
+(setq c-basic-offset 4)
+(setq c-indent-level 4)
+(setq tab-width 4)
 
-;; some interface niceties
-(setq confirm-kill-emacs                  'y-or-n-p
-      confirm-nonexistent-file-or-buffer  t
-      mouse-yank-at-point                 t
-      inhibit-startup-message             t
-      inhibit-startup-echo-area-message   t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq-default fill-column 80)
+
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+(display-time-mode t)
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
 
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 
-;; by default tooltips open in seperate frame. to force emacs to use the echo
-;; area exclusively uncomment the following
-;; (tooltip-mode -1)
-;; (setq tooltip-use-echo-area t)
+(setq confirm-kill-emacs         'y-or-n-p
+      inhibit-startup-message    t)
 
 (provide 'laydros-base)
 ;; base ends here
