630f46acb6002c63e26676dd711f22fd99678615
Jason Hamilton <jwh@laydros.net>
2017-09-19 09:39:50 -0400

    added markdown mode and ssh default for tramp to emacs

--- emacs/init.el
+++ emacs/init.el
@@ -20,7 +20,7 @@
   (package-install 'use-package))
 (require 'use-package)
 
-
+(setq tramp-default-method "ssh")
 (load-theme 'solarized-dark t)
 
 ;; UTF-8
@@ -82,7 +82,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package solarized-theme company))))
+ '(package-selected-packages (quote (markdown-mode use-package solarized-theme company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
