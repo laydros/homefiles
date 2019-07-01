f8b8c03038f1d6e38b007e5d59798a2c7de89c2f
Jason Hamilton <jwh@laydros.net>
2019-07-01 11:05:16 -0400

    moved linux specific symlink-files stuff to function to be commented when run on non-linux

--- bin/symlink-files.sh
+++ bin/symlink-files.sh
@@ -33,14 +33,27 @@ function symlink_file () {
     fi
 }
 
+function linux_specific () {
+    mkdir -p $CONFIG_DIR/i3
+    mkdir -p $CONFIG_DIR/i3status
+
+    symlink_file $I3CONFIG $HOMEFILES/config/i3config
+    symlink_file $I3STATUS $HOMEFILES/config/i3status.conf
+
+    symlink_file $XBINDRC $HOMEFILES/xbindkeysrc
+    symlink_file $XBINDMPD $HOMEFILES/xbindkeysrc-mpd
+    symlink_file $XBINDSPOT $HOMEFILES/xbindkeysrc-spotify
+    symlink_file $XRESOURCES $HOMEFILES/config/Xresources
+    symlink_file $AUTOKEYDIR $HOMEFILES/autokey
+}
+
 echo "Config dir is $CONFIG_DIR and homefiles source is $HOMEFILES"
 echo "Create a few directories if they don't exist"
 #mkdir -p ~/Maildir
 mkdir -p ~/.emacs.d
 touch $HOME/emacs.d/custom.el
+mkdir -p $CONFIG_DIR
 mkdir -p $CONFIG_DIR/nvim
-mkdir -p $CONFIG_DIR/i3
-mkdir -p $CONFIG_DIR/i3status
 mkdir -p $CONFIG_DIR/git
 mkdir -p $CONFIG_DIR/nano
 echo "Creating symlinks for dotfiles:"
@@ -49,21 +62,14 @@ symlink_file $LAYDROS_LISP $HOMEFILES/emacs/laydros-lisp
 # XDG
 symlink_file $GITCONFIG $HOMEFILES/config/gitconfig
 symlink_file $GITIGNORE $HOMEFILES/config/gitignore
-symlink_file $I3CONFIG $HOMEFILES/config/i3config
-symlink_file $I3STATUS $HOMEFILES/config/i3status.conf
 symlink_file $NANORC $HOMEFILES/config/nanorc
 symlink_file $VIMRC $HOMEFILES/config/init.vim
-
-symlink_file $XBINDRC $HOMEFILES/xbindkeysrc
-symlink_file $XBINDMPD $HOMEFILES/xbindkeysrc-mpd
-symlink_file $XBINDSPOT $HOMEFILES/xbindkeysrc-spotify
-symlink_file $XRESOURCES $HOMEFILES/config/Xresources
 symlink_file $ZSHRC $HOMEFILES/config/zshrc
 #symlink_file $OFFLINEIMAPRC ~/code/homefiles/offlineimaprc
 
 # directories
-symlink_file $AUTOKEYDIR $HOMEFILES/autokey
 symlink_file $BIN_DIR $HOMEFILES/bin
 symlink_file $MUTTDIR $HOMEFILES/config/neomutt
 
+# linux_specific
 echo "you may want to run bin/lappy.sh to setup fstab"
