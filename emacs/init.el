f9ab0ee0e5cda06379e93351345575c8f10b9202
Jason Hamilton <jwh@laydros.net>
2019-08-22 14:56:01 -0400

    emacs font to 12, adding mac setup guide

--- emacs/init.el
+++ emacs/init.el
@@ -25,7 +25,7 @@
 (setq user-mail-address "jwh@laydros.net")
 
 (add-to-list 'default-frame-alist
-             '(font . "Inconsolata-11"))
+             '(font . "Inconsolata-12"))
 
 ;;
 ;; interface
