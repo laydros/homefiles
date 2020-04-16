17044cfe77d4db22a4440c0d92284a6bc0d1d177
Jason Hamilton <jwh@laydros.net>
2020-04-16 12:22:06 -0400

    more gnome keybindings to deb-post-install. some may be ubuntu

--- bin/deb-post-install.sh
+++ bin/deb-post-install.sh
@@ -64,9 +64,14 @@ LIST=$LIST" galculator "
 #----------------------
 
 # for gnome and unity, allow resize with right button
-echo "Allow gnome resize with right mouse button"
+echo "Allow gnome resize with right mouse button, set alt as modifier"
 gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier '<Alt>'
+
+echo "Set gnome to use emacs style keybindings"
+gsettings set org.gnome.desktop.interface gtk-key-theme 'Emacs'
+
+echo "Set gnome workspace and window management to a mix of pop-os and regolith style"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Control><Shift><Alt>j', '<Super><Shift>Down']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Control><Shift><Alt>k', '<Super><Shift>Up']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Down']"
@@ -81,10 +86,14 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
 gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Primary><Super>Down']"
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Primary><Super>Up']"
-gsettings set org.gnome.desktop.interface gtk-key-theme 'Emacs'
+
 gsettings get org.gnome.mutter.keybindings toggle-tiled-left "['<Primary><Super>Left']"
 gsettings get org.gnome.mutter.keybindings toggle-tiled-right  "['<Primary><Super>Right']"
 
+echo "Set some gnome launch shortcuts to regolith style"
+gsettings get org.gnome.settings-daemon.plugins.media-keys control-center "['<Super>c']"
+gsettings get org.gnome.settings-daemon.plugins.media-keys terminal "['<Super>Return']"
+gsettings get org.gnome.settings-daemon.plugins.media-keys www "['<Super><Shift>Return']"
 
 # apt-get update
 
