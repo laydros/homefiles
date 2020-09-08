646ed42ebc1100e2362902f41df4436285501cd2
Jason Hamilton <jwh@laydros.net>
2020-09-08 12:41:30 -0400

    adding notes to home-backup and another example

--- bin/home-backup.sh
+++ bin/home-backup.sh
@@ -3,6 +3,8 @@
 DATE=$(date +%Y-%m-%d)
 BACKUP_DIR="/home/bak"
 
+## -c indicates creating new archive, -z filters through gzip, -f is file name
+
 tar -zcvpf $BACKUP_DIR/laydros-backup-$DATE.tar.gz \
     --exclude='/home/laydros/dl/iso' \
     --exclude=.cache \
@@ -16,3 +18,23 @@ tar -zcvpf $BACKUP_DIR/laydros-backup-$DATE.tar.gz \
 
 # Delete files older than 10 days
 # find $BACKUP_DIR/* -mtime +10 -exec rm {} \;
+
+# could also put place to backup at the end like this example:
+
+# lsblk
+# sudo mount /dev/sba1 /mnt/Toshiba/
+# tar -cvz \
+# --exclude="VirtualBox VMs" \
+# --exclude="node_modules" \
+# --exclude="venv" \
+# --exclude=".cache" \
+# --exclude=".debug" \
+# --exclude="Applications" \
+# --exclude="google-cloud-sdk" \
+# --exclude=".zoom" \
+# --exclude=".nuget" \
+# --exclude="styleImages" \
+# --exclude=".android" \
+# --exclude="mini-kube" \
+# --exclude="Steam" \
+# -f /mnt/Toshiba/06-07.tar.gz /home/gautierk
